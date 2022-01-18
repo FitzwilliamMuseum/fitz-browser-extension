bafce9fb4b2f3caa2995a50067e986dcc3c6d801
Daniel Pett <dejp3@cam.ac.uk>
2022-01-18 15:03:30 +0000

    Fix analytics

--- docs/js/analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-QGK1BLXR9N"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-QGK1BLXR9N');
-</script>
