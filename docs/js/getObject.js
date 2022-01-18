8a6be0e23c399c1a01d6565630504da19b164293
Daniel Pett <dejp3@cam.ac.uk>
2022-01-18 13:01:58 +0000

    Amend layout

--- docs/js/getObject.js
+++ docs/js/getObject.js
@@ -15,18 +15,25 @@ request.addEventListener('load', function (e) {
     const image = store + response.multimedia[0].processed.large.location;
     const url = response.admin.uri;
     const maker = response.lifecycle.creation;
+
     document.getElementById('title').innerHTML = title;
-    document.getElementById('fitzmuseumimage').innerHTML = '<img class="border border-white border-5" src="' + image + '">';
+
     var makers = document.getElementById("makers");
     for(var i=0; i < maker.length; i++)
       var creator = maker[i]["maker"][0]["summary_title"];
-      makers.innerHTML += "<p>"+ creator +"</p>";
+      makers.innerHTML += creator;
+
     var pinterest = document.getElementById('pinterest');
     var twitter = document.getElementById('twitter');
     var facebook = document.getElementById('facebook');
     var discover = document.getElementById('discover');
+    var imageElement = document.getElementById('object');
+
     pinterest.href = 'http://pinterest.com/pin/create/button/?url=' + url  + '&media=' + image +  '&description=' + title;
     twitter.href = 'http://www.twitter.com/intent/tweet/?text=' + encodeURIComponent(title) + '&url=' + url  + '&via=fitzmuseum_uk';
     facebook.href = 'https://www.facebook.com/sharer/sharer.php?u=' + url;
     discover.href = url;
+    imageElement.src = image;
+    imageElement.class = "";
+    
 });
